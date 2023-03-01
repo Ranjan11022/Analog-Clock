a8470257f98ab4efb64a3fc562d18aeb226a4652
Ranjan11022 <ranjankr11022000@gmail.com>
2023-03-01 16:39:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let hr = document.getElementById('hour')
+let min = document.getElementById('min')
+let sec = document.getElementById('sec')
+
+
+function displayTime() {
+    let date = new Date();
+
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let hRotate = 30 * hh + mm / 2;
+    let mRotate = 6 * mm;
+    let sRotate = 6 * ss;
+
+    hr.style.transform = `rotate(${hRotate}deg)`;
+    min.style.transform = `rotate(${mRotate}deg)`;
+    sec.style.transform = `rotate(${sRotate}deg)`;
+
+
+}
+setInterval(displayTime, 1000);
